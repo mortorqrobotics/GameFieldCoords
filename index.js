5b318c6858bf0656ce9183bea0d45274ebf37277
Cameron <meme34903@gmail.com>
2022-01-23 16:43:01 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let fieldWidth = 1646;
+let fieldHeight = 823;
+
+let x;
+let y;
+
+document.addEventListener('mousemove', (e) => {
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+    let elementMouseIsOver = document.elementFromPoint(mouseX, mouseY);
+    if(elementMouseIsOver.tagName !== "IMG") return;
+
+    let {x: xOffset, y: yOffset, width, height } = elementMouseIsOver.getBoundingClientRect();
+    x = mouseX - xOffset;
+    y = mouseY - yOffset;
+
+    document.querySelector('.coords').textContent = `x: ${x}px, y: ${y}px`;
+    document.querySelector('.pos').textContent = `x: ${((x / width) * fieldWidth).toFixed(2)}cm y: ${((y / height) * fieldHeight).toFixed(2)}cm`;
+})
+
+document.addEventListener('click', (e) => {
+    let { width, height } = document.querySelector('img').getBoundingClientRect();
+    document.querySelector('#savePos').textContent = `x: ${((x / width) * fieldWidth).toFixed(2)}cm y: ${((y / height) * fieldHeight).toFixed(2)}cm`
+})
