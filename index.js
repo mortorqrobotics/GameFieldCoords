ec5e98262874782117a2ede65a9e4c76e40923d5
Cameron <meme34903@gmail.com>
2022-01-28 17:46:22 -0800

    distance calculating

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ document.addEventListener('click', (e) => {
     let { width, height } = document.querySelector('img').getBoundingClientRect();
     document.querySelector('#savePos').innerHTML += `<br>x: ${((x / width) * fieldWidth).toFixed(2)}cm y: ${((y / height) * fieldHeight).toFixed(2)}cm`
 })
+
+const roundTo = (num, place) => {
+    return Math.round(num * (10 * place)) / (10 * place);
+}
+
+const convertToCm = (num, x=true) => {
+    let total;
+    let image;
+    let { width, height } = document.querySelector('img').getBoundingClientRect();
+    if(x) {
+        total = fieldWidth
+        image = width;
+    } else {
+        total = fieldHeight;
+        image = height;
+    }
+    return (num / image) * total;
+}
+
+let positions = [];
+document.addEventListener('mousedown', (e) => {
+    if(e.button !== 1) return; // middle click
+
+    if(!positions[0]) return positions[0] = [convertToCm(x), convertToCm(y, false)];
+    if(!positions[1]) positions[1] = [convertToCm(x), convertToCm(y, false)];
+
+    console.log("distance", `${roundTo(Math.hypot(positions[1][0] - positions[0][0], positions[1][1] - positions[0][1]) / 100, 2)} meters`)
+    positions = [];
+})
