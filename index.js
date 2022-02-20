d20611c9a8723d7cf143a52d0788ee835f834d39
Cameron <43302238+biggant1@users.noreply.github.com>
2022-02-20 13:27:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ document.addEventListener('mousemove', (e) => {
     document.querySelector('.pos').textContent = `x: ${((x / width) * fieldWidth).toFixed(2)}cm y: ${((y / height) * fieldHeight).toFixed(2)}cm`;
 })
 
-document.addEventListener('click', (e) => {
+/*document.addEventListener('click', (e) => {
     if(e.target.tagName !== "IMG") return;
     let { width, height } = document.querySelector('img').getBoundingClientRect();
     document.querySelector('#savePos').innerHTML += `<br>x: ${((x / width) * fieldWidth).toFixed(2)}cm y: ${((y / height) * fieldHeight).toFixed(2)}cm`
-})
+})*/
 
 const roundTo = (num, place) => {
     return Math.round(num * (10 * place)) / (10 * place);
@@ -44,7 +44,7 @@ const convertToCm = (num, x=true) => {
 
 let positions = [];
 document.addEventListener('mousedown', (e) => {
-    if(e.button !== 1) return; // middle click
+    if(e.button !== 0) return; // middle click
 
     if(!positions[0]) return positions[0] = [convertToCm(x), convertToCm(y, false)];
     if(!positions[1]) positions[1] = [convertToCm(x), convertToCm(y, false)];
